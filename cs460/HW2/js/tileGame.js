c81f48b477179567876f5b9fd1b609e732639834
Alex Molodyh <amolodyh16@wou.edu>
2017-10-07 20:53:34 -0700

    js working on laptop but with the same warnings

---
+++ cs460/HW2/js/tileGame.js
@@ -0,0 +1,9 @@
+/*gloval $, jQuery, alert*/
+
+
+function generateClick() {
+    var row = $("#rows").val().trim();
+    var col = $("#columns").val().trim();
+    //alert("Row: " + row + "\nColumn: " + col);
+    
+}
