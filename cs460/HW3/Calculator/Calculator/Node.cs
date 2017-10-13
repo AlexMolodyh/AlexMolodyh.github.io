dd1ac08f3c16426f0e33c643a530e0e0e3f5c504
= <amolodyh16@mail.wou.edu>
2017-10-12 19:04:59 -0700

    fixed calculator project issue

---
+++ cs460/HW3/Calculator/Calculator/Node.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculator
+{
+    public class Node
+    {
+        public object Data;
+        public Node Next;
+
+        public Node()
+        {
+            Data = null;
+            Next = null;
+        }
+
+        public Node(object data, Node next)
+        {
+            this.Data = data;
+            this.Next = next;
+        }
+    }
+}
