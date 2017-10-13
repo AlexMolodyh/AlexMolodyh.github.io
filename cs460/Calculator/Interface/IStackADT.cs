53a496cec3f36886ed347a04323b78c65048f3a6
= <amolodyh16@mail.wou.edu>
2017-10-12 18:15:01 -0700

    before going back in time

---
+++ cs460/Calculator/Interface/IStackADT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculator.Interface
+{
+    public interface IStackADT<T>
+    {
+        T Push(T newItem);
+        T Pop();
+        T Peek();
+        bool IsEmpty();
+        void Clear();
+    }
+}
