de6adcb193670042621e2ab1efa71c391b1c4a2f
AlexMolodyh <amolodyh16@mail.wou.edu>
2017-10-22 00:22:20 -0700

    trying to pass data to action method

--- cs460/HW4/HW4/Controllers/HomeController.cs
+++ cs460/HW4/HW4/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -28,6 +29,38 @@ namespace HW4.Controllers
         }
 
         public ActionResult PageOne()
+        {
+            string temperature = Request.Form["temperatue"];
+            string temp_type = Request.Form["temp_type"];
+            double tempToConvert = 0.0;
+            double converted = 0.0;
+
+            ViewBag.converted = temperature;
+            Console.WriteLine("Hello " + temperature);
+
+
+            if (Double.TryParse(temperature, out tempToConvert))
+            {
+                if (temp_type.Equals("F"))
+                    converted = (((tempToConvert - 32) * 5) / 9);
+                else if (temp_type.Equals("C"))
+                    converted = (((tempToConvert * 9) / 5) + 32);
+                else
+                    converted = 0.0;
+
+                ViewBag.converted = converted;
+            }
+
+
+            return View();
+        }
+
+        public ActionResult PageTwo()
+        {
+            return View();
+        }
+
+        public ActionResult PageThree()
         {
             return View();
         }
