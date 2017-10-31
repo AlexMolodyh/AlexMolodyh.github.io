b74242f6e1a5c3b0901de1e2f38d939cd930d400
AlexMolodyh <amolodyh16@mail.wou.edu>
2017-10-31 05:47:26 -0700

    added database layer

---
+++ cs460/HW5/HW5/HW5/App_Data/db_UP.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE dbo.Customers
+(
+	ID INT IDENTITY (1, 1) NOT NULL,
+	CustomerNumber INT NOT NULL,
+	FirstName NVARCHAR(100) NOT NULL,
+	MiddleName NVARCHAR(100) NOT NULL,
+	LastName NVARCHAR(100) NOT NULL,
+	DOB DATETIME NOT NULL,
+	NewAddress NVARCHAR(300) NOT NULL,
+	NewCity NVARCHAR(80) NOT NULL,
+	NewState NVARCHAR(80) NOT NULL,
+	NewZip INT NOT NULL,
+	NewCounty NVARCHAR(50) NOT NULL,
+	ChangeDate DATETIME NOT NULL,
+	CONSTRAINT [PK_dbo.Customers] PRIMARY KEY CLUSTERED (ID ASC)
+);
+
+INSERT INTO dbo.Customers
+        ( CustomerNumber ,
+          FirstName ,
+          MiddleName ,
+          LastName ,
+          DOB ,
+          NewAddress ,
+          NewCity ,
+          NewState ,
+          NewZip ,
+          NewCounty ,
+          ChangeDate
+        )
+VALUES  ( 0 , 'Homer' , 'J' , 'Simpson' , '1958' , '12345 New Springfield St' , 'New Springfield' , 'Oregon' , 97403 , 'Lane County' , GETDATE()),
+		( 0 , 'Marge' , 'J' , 'Simpson' , '1960' , '12345 New Springfield St' , 'New Springfield' , 'Oregon' , 97403 , 'Lane County' , GETDATE()),
+		( 0 , 'Bart' , 'J' , 'Simpson' , '1980' , '12345 New Springfield St' , 'New Springfield' , 'Oregon' , 97403 , 'Lane County' , GETDATE()),
+		( 0 , 'Lisa' , 'J' , 'Simpson' , '1982' , '12345 New Springfield St' , 'New Springfield' , 'Oregon' , 97403 , 'Lane County' , GETDATE()),
+		( 0 , 'Maggie' , 'J' , 'Simpson' , '1989' , '12345 New Springfield St' , 'New Springfield' , 'Oregon' , 97403 , 'Lane County' , GETDATE());
---
+++ cs460/HW5/HW5/HW5/Models/Customer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HW5.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Customer Number")]
+        public int CustomerNumber { get; set; }
+
+        [Required, StringLength(100)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required, StringLength(100)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Required, StringLength(100)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Date Of Birth")]
+        public DateTime DOB { get; set; }
+
+        [Required, StringLength(300)]
+        [Display(Name = "New Address")]
+        public string NewAddress { get; set; }
+
+        [Required, StringLength(80)]
+        [Display(Name = "City")]
+        public string NewCity { get; set; }
+
+        [Required, StringLength(80)]
+        [Display(Name = "State")]
+        public string NewState { get; set; }
+
+        [Required]
+        [Display(Name = "Zip Code")]
+        public int NewZip { get; set; }
+
+        [Required, StringLength(50)]
+        [Display(Name = "County")]
+        public string NewCounty { get; set; }
+
+        [Required]
+        [Display(Name = "Date of Change")]
+        public DateTime ChangeDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}: {FirstName} {MiddleName} {LastName} DOB = {DOB}";
+        }
+    }
+}
