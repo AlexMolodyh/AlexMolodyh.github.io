ef7496f73fd4e0b062376641969273d1dad172dc
Alex Molodyh <amolodyh16@mail.wou.edu>
2017-11-19 22:50:59 -0800

    commented all code

--- cs460/HW7/HW7/HW7/Controllers/GiphyController.cs
+++ cs460/HW7/HW7/HW7/Controllers/GiphyController.cs
@@ -1,4 +1,12 @@
-﻿using HW7.DAL;
+﻿/*
+ * Author: Alexander Molodyh
+ * Date: 11/19/2017
+ * Class: CS460
+ * Assignment: HW7
+ */
+
+
+using HW7.DAL;
 using HW7.Models;
 using Newtonsoft.Json;
 using System;
@@ -12,74 +20,77 @@ using System.Web.Mvc;
 
 namespace HW7.Controllers
 {
+    /// <summary>
+    /// GiphyController retrieves a Gifs from Giphy.com and sends them back to the requester.
+    /// </summary>
     public class GiphyController : Controller
     {
         GifRequestContext db = new GifRequestContext();
 
+        /// <summary>
+        /// Sends a GET request to giphy.com and recieves a Json object of gifs.
+        /// The gifs are then filtered and sent back to the client.
+        /// </summary>
+        /// <param name="searchArea">The area to search for gifs in such as: search, trending, and random</param>
+        /// <param name="searchParams">Something to search for in the search area. Ex: cheeseburgers, dragons, cars..</param>
+        /// <param name="rating">A rating to filter the gif output with such as: Rated G, PG, PG-13, or All</param>
+        /// <returns>A Json object that contains a list of gif objects</returns>
         [HttpGet]
         public JsonResult GetJsonGifs(string searchArea, string searchParams, string rating)
         {
-            //Get the 
+            //Get the IP address and browser user agent for logging.
             string ipAddress = Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
             string userAgent = Request.Headers["User-Agent"].ToString();
-
             if(string.IsNullOrEmpty(ipAddress))
-            {
                 ipAddress = Request.ServerVariables["REMOTE_ADDR"];
-                for(int i = 0; i < Request.ServerVariables.Count; i++)
-                {
-                    Debug.WriteLine($"Address: {Request.ServerVariables[i]}");
-                }
-            }
 
+            //Get api key and build the GET request header.
             string apiKey = System.Web.Configuration.WebConfigurationManager.AppSettings["GiphyKey"];
             string url = $"http://api.giphy.com{searchArea}?api_key={apiKey}&q={searchParams}";
             Debug.WriteLine($"Url is: {url}");
             GifList gl = new GifList();
+            GiphyObj giphyObjs = null;
 
+            /*Try to deserialize the json data into C# objects*/
             try
             {
+                //Send request to Giphy.com
                 HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(url);
                 httpRequest.Method = "GET";
                 httpRequest.ContentType = "application/json";
 
+                //Handle response
                 HttpWebResponse response = (HttpWebResponse)httpRequest.GetResponse();
                 StreamReader sr = new StreamReader(response.GetResponseStream());
                 var result = sr.ReadToEnd();
                 Debug.WriteLine("Result is: " + result);
 
-                GiphyObj giphyObjs = JsonConvert.DeserializeObject<GiphyObj>(result, Converter.Settings);
-
-                Debug.WriteLine("The data is: " + giphyObjs.Data[0].Images.OriginalStill.Url);
-                Debug.WriteLine($"Data count is: {giphyObjs.Data.Count}");
-                gl = GetGifs(giphyObjs, rating);
+                giphyObjs = JsonConvert.DeserializeObject<GiphyObj>(result, Converter.Settings);
             }
-            catch (Exception e){}
-            
-            GifRequest giphyRequest = new GifRequest()
-            {
-                IPAddress = ipAddress,
-                BrowserType = userAgent,
-                RequestDate = DateTime.Now,
-                SearchType = searchArea,
-                Rating = rating,
-                KeyWord = searchParams
-            };
+            catch (Exception e){ Debug.WriteLine(e.StackTrace); }
 
-            LogRequest(giphyRequest);//Log user request
+            gl = GetGifs(gl, giphyObjs, rating);
+            LogRequest(ipAddress, userAgent, searchArea, searchParams, rating);//Log user request
 
             return Json(gl, JsonRequestBehavior.AllowGet);
         }
 
-        private GifList GetGifs(GiphyObj giphyObj, string rating)
+        /// <summary>
+        /// Filters the gifs based on the client rating selection.
+        /// </summary>
+        /// <param name="gifList">A GifList to populate with Gifs</param>
+        /// <param name="giphyObj">The GiphyObj containing Json gifs that were serialized</param>
+        /// <param name="rating">The rating to filter the gifs with</param>
+        /// <returns></returns>
+        private GifList GetGifs(GifList gifList, GiphyObj giphyObj, string rating)
         {
-            GifList gifList = new GifList();
-            int size = giphyObj.Data.Count;
+            if (giphyObj == null)
+                return null;
 
+            int size = giphyObj.Data.Count;
             List<Gif> gifs = new List<Gif>(size);
 
-            Debug.WriteLine($"Rating: {rating}");
-
+            //filters the gifs
             for (int i = 0; i < giphyObj.Data.Count; i++)
             {
                 if (rating.Equals("all"))
@@ -94,11 +105,14 @@ namespace HW7.Controllers
                     }
                 }
             }
+
+            //add the gif list to the GifList object
             gifList.Gifs = gifs;
             gifList.Size = gifList.Gifs.Count;
             return gifList;
         }
 
+        //Adds a gif to the list of gifs
         private List<Gif> AddGif(GiphyObj giphyObj, int i, List<Gif> gifs)
         {
             Gif myGif = new Gif();
@@ -109,11 +123,22 @@ namespace HW7.Controllers
             return gifs;
         }
 
-        private void LogRequest(GifRequest gif)
+        //Inserts a client request into the database.
+        private void LogRequest(string ipAddress, string userAgent, string searchArea, string searchParams, string rating)
         {
+            GifRequest giphyRequest = new GifRequest()
+            {
+                IPAddress = ipAddress,
+                BrowserType = userAgent,
+                RequestDate = DateTime.Now,
+                SearchType = searchArea,
+                Rating = rating,
+                KeyWord = searchParams
+            };
+
             try
             {
-                db.GifRequests.Add(gif);
+                db.GifRequests.Add(giphyRequest);
                 db.SaveChanges();
             }
             catch(Exception e)
--- cs460/HW7/HW7/HW7/Scripts/asyncWorker.js
+++ cs460/HW7/HW7/HW7/Scripts/asyncWorker.js
@@ -1,16 +1,33 @@
-﻿function search() {
+﻿/**
+ * Author: Alexander Molodyh
+ * Date: 11/19/2017
+ * Class: CS460
+ * Assignment: HW7
+ */
 
+
+/*Performs a GET request to the GiphyController and populates the page with 
+ *gifs uppon success.*/
+function search() {
+    //search parameters
     var searchP = $('#search-box').val();
+
+    //area to search in such as: /v1/gifs/search, /v1/gifs/trending, or /v1/gifs/random
     var currentSearchArea = "/v1/gifs/search";
+
+    //the rating to filter the return gifs with
     var currentRating = "all";
 
+    //check if the search area has been modified. If yes then change the currentSearchArea
     if (document.searchArea)
         currentSearchArea = "/v1/gifs/" + document.searchArea;
 
+    //if the rating has been modified then set currentRating to the chosen rating.
     if (document.selectedRating) {
         currentRating = document.selectedRating;
     }
 
+    //call the controller with the search parameters
     $.ajax({
         type: "get",
         datatype: "json",
@@ -18,7 +35,7 @@
         data: { searchArea: currentSearchArea, searchParams: searchP, rating: currentRating },
         success: function (data) {
             if (data.Size > 0)
-                populateGifs(data);
+                populateGifs(data);//populate gifs onto the page
             else
                 alert("No results found!");
         },
@@ -28,33 +45,42 @@
     });
 }
 
+
+/**
+ * Builds a row with unordered lists of gifs and displays them on the Index page.
+ * @param {any} gifList Is a Json object that represents a GifList. A GifList contains a list of Gif objects
+ * that have a Url for the gif and a username.
+ */
 function populateGifs(gifList) {
     var container = $("#gif-container");
     container.empty();
 
     var gifListSize = gifList.Size;
-    var imgPerUl = Math.ceil(gifListSize / 4);
-   
+    var imgPerUl = Math.ceil(gifListSize / 4);//The amount of gifs each <ul> should have
+
+    //the first loop builds columns containing an unordered list of gifs
     var row = $("<div></div>").attr("class", "row");
     for (var i = 0; i < gifList.Size; i++) {
         var ul = $("<ul></ul>").css("list-style-type", "none");/*ul for a single column*/
         var column = $("<div></div>").attr("class", "col-md-3").attr("id", "gif" + j);
 
+        //second loop populates each unordered list with gifs
         var j;
         for (j = i; j < i + imgPerUl && j < gifList.Size; j++) {
-            var gif = gifList.Gifs[j];
+            var gif = gifList.Gifs[j];//get the current gif
 
-            var gifImageUrl = gif.Url;
-            var imgDiv = $("<div></div>").attr("class", "gif-image-div");
+            var gifImageUrl = gif.Url;//get the gif url
+            var imgDiv = $("<div></div>").attr("class", "gif-image-div");//holds the gif and username
             var li = $("<li></li>");
 
+            //holds the gif image
             var gifImage = $("<img />")
                 .attr("src", gifImageUrl)
                 .attr("class", "gif-image")
                 .attr("id", "img" + j)
                 .css("max-width", "100%");
 
-            var h5;
+            var h5;//checks if the usernaem isn't null and populates the usename section
             if (gif.Username) {
                 h5 = $("<h5>By " + gif.Username + "</h5>")
                     .css("max-width", "100%")
@@ -74,7 +100,7 @@ function populateGifs(gifList) {
     container.append(row);
 }
 
-var num = null;
+/*This function registers the Rating and Search Area selections*/
 $(".btn-group > button.btn").on("click", function () {
     var buttonType = this.innerHTML.substring(0, 5);
 
@@ -86,7 +112,7 @@ $(".btn-group > button.btn").on("click", function () {
         document.searchArea = this.innerHTML.toLowerCase();
         if (document.searchArea.toLowerCase() === "search") {
             $("#search-box").show();
-            $("#search-button").css("min-width", "60px");
+            $("#search-button").css("min-width", "110px");
             $("#search-gifs").text("Search Gifs");
         }
         else {
