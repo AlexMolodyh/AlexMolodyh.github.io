993dd70642c16fe82f8761d9e8d6012c3482a460
Alex Molodyh <amolodyh16@mail.wou.edu>
2017-11-25 18:54:33 -0800

    done with artist_db_up

---
+++ cs460/HW8/HW8/HW8/App_Data/ArtWork_UP.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE dbo.ARTWORK 
+(
+	ArtWorkTitle NVARCHAR(50) NOT NULL,
+	Artist NVARCHAR(50) NOT NULL
+	CONSTRAINT [PK_dbo.ArtWorkTitle] PRIMARY KEY CLUSTERED (ArtWorkTitle ASC),
+	CONSTRAINT [FK_dbo.Artist] FOREIGN KEY (Artist) REFERENCES dbo.ARTIST(ArtistName)
+);
+GO
+
+INSERT INTO dbo.ARTWORK
+(
+    ArtWorkTitle,
+    Artist
+)
+VALUES
+(   'Circle Limit III', 'M.C. Escher' )
