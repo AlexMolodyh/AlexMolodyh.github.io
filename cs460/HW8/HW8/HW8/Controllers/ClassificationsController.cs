95ce35092a806da4455dbbc55f15bc893a925f6e
Alex Molodyh <amolodyh16@mail.wou.edu>
2017-11-26 19:37:50 -0800

    added table to classification and artworks

--- cs460/HW8/HW8/HW8/Controllers/ClassificationsController.cs
+++ cs460/HW8/HW8/HW8/Controllers/ClassificationsController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Data.Entity;
+using HW8.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,82 +10,13 @@ namespace HW8.Controllers
 {
     public class ClassificationsController : Controller
     {
+        public ArtDBContext db = new ArtDBContext();
+
         // GET: Classifications
         public ActionResult Index()
         {
-            return View();
-        }
-
-        // GET: Classifications/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Classifications/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Classifications/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Classifications/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Classifications/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Classifications/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Classifications/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            var artWorks = db.ArtWorks.Include(a => a.Artist1);
+            return View(artWorks.ToList());
         }
     }
 }
