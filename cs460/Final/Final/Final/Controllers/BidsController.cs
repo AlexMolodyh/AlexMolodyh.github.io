53bfe27fc6a6eb64530c1d2c23861565b4f715b1
Alex Molodyh <amolodyh16@mail.wou.edu>
2017-12-05 18:18:50 -0800

    Before review

--- cs460/Final/Final/Final/Controllers/BidsController.cs
+++ cs460/Final/Final/Final/Controllers/BidsController.cs
@@ -63,65 +63,6 @@ namespace Final.Controllers
             return View(bid);
         }
 
-        // GET: Bids/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Bid bid = db.Bids.Find(id);
-            if (bid == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.BuyerName = new SelectList(db.Buyers, "BuyerName", "BuyerName", bid.BuyerName);
-            return View(bid);
-        }
-
-        // POST: Bids/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ItemID,BuyerName,Price,TimeStamp")] Bid bid)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(bid).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.BuyerName = new SelectList(db.Buyers, "BuyerName", "BuyerName", bid.BuyerName);
-            return View(bid);
-        }
-
-        // GET: Bids/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Bid bid = db.Bids.Find(id);
-            if (bid == null)
-            {
-                return HttpNotFound();
-            }
-            return View(bid);
-        }
-
-        // POST: Bids/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Bid bid = db.Bids.Find(id);
-            db.Bids.Remove(bid);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
